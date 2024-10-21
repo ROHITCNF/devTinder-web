d8cfeac5fec6a564eb47f9956c97effe9b38af68
Rohit Srivastava <Rohitcnf@gmail.com>
2024-10-22 01:48:32 +0530

    First Commit : Hello DevTinder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+/*
+  All the thing will be inside the App component 
+
+*/
+const App = () => {
+  return <div>Dev Tinder Web</div>;
+};
+
+export default App;
